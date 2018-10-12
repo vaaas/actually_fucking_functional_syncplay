b8b28a5608e07f9536d2dd5546ec9f6a3cba3d66
Vasileios Pasialiokis <whiterocket@outlook.com>
2018-10-12 21:25:07 +0300

    initial commit

---
+++ client.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+// jshint asi: true
+// jshint esversion: 6
+
+const child_process = require("child_process")
+const net = require("net")
+const ws = require("ws")
+const socket_path = "/tmp/mpvsocket.sock"
+
+function WebSocketClient(ip, port, mpv) {
+	const connection = new ws(`ws://${ip}:${port}/`)
+	connection.on("open", () => {
+		console.log("connected to websocket server") })
+	connection.on("message", on_message)
+	
+	function on_message(what) {
+		console.log(what)
+		switch(what) {
+			case "play":
+				mpv.play()
+				break
+			case "pause":
+				mpv.pause()
+				break
+			case "quit":
+				process.exit(0)
+				break }}}
+
+function MPV(path) {
+	const socket = new net.Socket()
+	socket.setMaxListeners(0)
+	connect()
+	socket.on("close", reconnect)
+	
+	function connect() {
+		socket.connect({path: path}, () => { console.log("connected to mpv socket") }) }
+		
+	function reconnect() {
+		console.log("Lost connection to socket, attempting to reconnect")
+		socket.end()
+		connect() }
+	
+	function play() {
+		send({ "command": ["set_property_string", "pause", "no"] }) }
+	
+	function pause() {
+		send({ "command": ["set_property_string", "pause", "yes"] }) }
+	
+	function send(what) {
+		socket.write(JSON.stringify(what) + "\n") }
+	
+	return { play: play, pause: pause } }
+
+function main() {
+	const ip = process.argv[2]
+	const file = process.argv[3]
+	let ready = false
+	const mpv_proc = child_process.spawn("mpv", [file, "--pause", `--input-ipc-server=${socket_path}`])
+	mpv_proc.stdout.on("data", () => {
+		if (!ready) {
+			WebSocketClient(ip, 8001, MPV("/tmp/mpvsocket"))
+			ready = true }}) }
+	
+	
+
+main()
---
+++ server.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+// jshint asi: true
+// jshint esversion: 6
+
+const ws = require("ws")
+const readline = require("readline")
+
+function WebSocketServer(ip, port) {
+	const server = new ws.Server({ port: port, host: ip})
+	server.on("connection", on_connection)
+	console.log("websocket listening to", ip + ":" + port)
+	console.log("'play' to play, 'pause' to pause, 'quit' to quit")	
+	Readline(on_readline)
+	
+	function on_readline(message) {
+		switch(message) {
+			case "pause":
+				pause()
+				break
+			case "play":
+				play()
+				break
+			case "quit":
+				process.exit(0)
+				break }}
+
+	function on_connection(socket) {
+		console.log(socket._socket.remoteAddress, "connected")
+		socket.on("message", on_message) }
+
+	function on_message(message) { console.log("message") }
+	function play() { broadcast("play") }
+	function pause() { broadcast("pause") }
+	
+	function broadcast(what) {
+		server.clients.forEach(client => {
+			client.send(what) })}}
+
+function Readline (cb) {
+	const rl = readline.createInterface({
+		input: process.stdin, output: process.stdout })
+	ask()
+	function ask() { rl.question("affs > ", answer_listener) }
+	function answer_listener(answer) {
+		cb(answer)
+		ask() }}
+
+function main() {
+	const ip = process.argv[2] ? process.argv[2] : "0.0.0.0"
+	WebSocketServer(ip, 8001) }
+
+main()
